eb45756c47508e1455565ce7b1ecd3c56395680a
lubanrahat <lubanrahat.dev@gmail.com>
2026-04-17 16:36:52 +0600

    Implement reverseString function to reverse a given string

--- problem.ts
+++ problem.ts
@@ -1,1 +1,17 @@
-console.log("Hello, TypeScript 6.0!");
+// 1. Reverse a String
+
+function reverseString(str: string) : string {
+    let arr = str.split("");
+    console.log(arr)
+    let left = 0,right = arr.length-1;
+    while(left < right) {
+        [arr[left],arr[right]] = [arr[right],arr[left]];
+        left++;
+        right--;
+    }
+    return arr.join("");
+}
+
+const foo = "Rahat";
+
+// console.log(reverseString(foo))
